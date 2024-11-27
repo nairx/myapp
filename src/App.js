d3b8c25131cf19278d6570aff8eff8330f98de44
nayanatara07 <nayana2017tara@gmail.com>
2024-11-27 19:05:42 +0530

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import Cart from "./Cart";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Header />
+      <BrowserRouter>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Link to="/content">Home</Link> |<Link to="/cart">Cart</Link>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <hr></hr>
+        <Routes>
+          <Route index element={<Content />} />
+          <Route path="content" element={<Content />} />
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
-
-export default App;
