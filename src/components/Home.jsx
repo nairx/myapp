760d2d73c5fc46c3a52c5a9096ffcad85a233fcb
nairx <nairxprv@gmail.com>
2025-08-28 21:39:40 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 import "./Home.css";
 export default function Home(props) {
+  const [cart, setCart] = useState([]);
   const products = [
     {
       id: 1,
@@ -21,17 +23,53 @@ export default function Home(props) {
       price: 50,
     },
   ];
-  return (
-    <div className="row">
-      {products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <p>{product.desc}</p>
-          <h4>{product.price}</h4>
-          <button>Add to Cart</button>
-        </div>
-      ))}
 
+  const addToCart = (product) => {
+    const found = cart.find((value) => value.id === product.id);
+    if (!found) {
+      product.qty = 1;
+      setCart([...cart, product]);
+    }
+  };
+
+  const deleteItem = (id) => {
+    const newCart = cart.filter((value) => value.id !== id);
+    setCart(newCart);
+  };
+
+  const addQty = (id, qty) => {
+    const found = cart.find((value) => value.id === id);
+    if (found) {
+      found.qty = qty + 1;
+      // setCart([...cart,])
+      setCart([...cart, found]);
+    }
+  };
+  return (
+    <div>
+      <div className="row">
+        {products.map((product) => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.desc}</p>
+            <h4>{product.price}</h4>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+      <hr />
+      <div>
+        <h3>My Cart</h3>
+        {cart &&
+          cart.map((item) => (
+            <li key={item.id}>
+              {item.name}-{item.price}-<button>-</button>
+              {item.qty}
+              <button onClick={() => addQty(item.id, item.qty)}>+</button>
+              <button onClick={() => deleteItem(item.id)}>Delete</button>
+            </li>
+          ))}
+      </div>
     </div>
   );
 }
