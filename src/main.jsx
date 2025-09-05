f486ae5695baaf2d7821426fa100f2938026008a
nairx <nairxprv@gmail.com>
2025-09-05 21:06:56 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import Parent1 from "./components/Parent1.jsx";
 import C1 from "./components/C1.jsx";
 import C2 from "./components/C2.jsx";
 createRoot(document.getElementById("root")).render(<App />);
-
 {
   /* <Parent1>
     <C1 />
