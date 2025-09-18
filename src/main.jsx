27e453d1d6b8128d5353801727eb9b5539ef99eb
nairx <nairxprv@gmail.com>
2025-09-18 20:08:59 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import CRUD from "./components/CRUD.jsx";
 import ProductTest from "./components/ProductTest.jsx";
 createRoot(document.getElementById("root")).render(<ProductTest />);
 
